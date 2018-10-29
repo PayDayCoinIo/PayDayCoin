e564d69c4104d9c8ba83431d32e838e9f30bc7ba
Jack Portman <Jacknospampaydaycoin.io>
2018-10-29 20:43:42 +1000

    Clean debug message, check algorythm

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -3542,7 +3542,7 @@ bool CWallet::CreateCoinStake(const CKeyStore& keystore, unsigned int nBits, int
 		
     if(bMasterNodePayment) {
         //spork
-        if(!masternodePayments.GetBlockPayee(pindexPrev->nHeight+1, payee, vin)){
+        if(!masternodePayments.GetBlockPayee(pindexPrev->nHeight+1, masternodepayee, vin)){
             CMasternode* winningNode = mnodeman.GetCurrentMasterNode(1);
             if(winningNode && !fIsInitialDownload){
                 masternodepayee = GetScriptForDestination(winningNode->pubkey.GetID());
