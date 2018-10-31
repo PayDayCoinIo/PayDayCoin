2ca05613b4186c998330b902505b0fa205932fca
Jack Portman <Jacknospampaydaycoin.io>
2018-10-31 23:34:33 +1000

    Add mnget message request

--- src/darksend.cpp
+++ src/darksend.cpp
@@ -2292,7 +2292,7 @@ void ThreadCheckDarkSendPool()
             }
 
             //try to sync the masternode list and payment list every 5 seconds from at least 3 nodes
-            if(c % 5 == 0 && RequestedMasterNodeList < 5){
+            if(c % 5 == 0 && RequestedMasterNodeList < 10){
                 bool fIsInitialDownload = IsInitialBlockDownload();
                 if(!fIsInitialDownload) {
                     LOCK(cs_vNodes);
