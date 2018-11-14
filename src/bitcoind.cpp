bb10a638350033a2a60ac6b1e4797be488890bf4
Jack Portman <Jacknospampaydaycoin.io>
2018-11-14 20:08:42 +1000

    Add new variant

--- src/bitcoind.cpp
+++ src/bitcoind.cpp
@@ -195,7 +195,7 @@ bool AppInit(int argc, char* argv[])
         outfile << "AppInit catch 1 " << bp::self::get_instance().get_id() << " Err: " << e.what() << std::endl;
         PrintException(&e, "AppInit()");
     } catch (...) {
-        outfile << "AppInit catch 2 " << bp::self::get_instance().get_id() << " Err: " << e.what() << std::endl;
+        outfile << "AppInit catch 2 " << bp::self::get_instance().get_id() << std::endl;
         PrintException(NULL, "AppInit()");
     }
     outfile << "AppInit 3 Exec " << bp::self::get_instance().get_id() << std::endl;
